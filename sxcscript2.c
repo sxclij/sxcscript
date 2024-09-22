a82c58487f2556fd52d33217d937813fe4914037
sxclij <sxclij@gmail.com>
2024-09-22 01:58:07 +0000

    io

--- sxcscript2.c
+++ sxcscript2.c
@@ -1,5 +1,15 @@
+#include <fcntl.h>
 #include <unistd.h>
 
-int main() {
-    write(STDOUT_FILENO, "good morning world!\n", 21);
+#define script_path "testscript.txt"
+#define script_capacity (1 << 16)
+
+int main()
+{
+    char buf[script_capacity];
+    int fd = open(script_path, O_RDONLY);
+    int buf_n = read(fd, buf, sizeof(buf) - 1);
+    buf[buf_n] = '\0';
+    close(fd);
+    write(STDOUT_FILENO, buf, buf_n);
 }
