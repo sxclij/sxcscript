3dd9f93d05ead73666948d3226d6584f4d213fbe
sxclij <sxclij@gmail.com>
2024-09-23 08:30:13 +0000

    add exec. str to uint32_t

--- sxcscript2.c
+++ sxcscript2.c
@@ -11,29 +11,14 @@ enum script_kind {
     script_kind_push,
     script_kind_call,
     script_kind_ret,
-    script_kind_add,
-    script_kind_sub,
-    script_kind_mul,
-    script_kind_div,
-    script_kind_mod,
 };
 struct script_node {
     enum script_kind kind;
-    const char* s;
+    uint32_t s;
     struct script_node* prev;
     struct script_node* next;
 };
 
-struct script_node* script_parse_push(struct script_node** dst_itr, const char* s, enum script_kind kind) {
-    struct script_node* prev = *dst_itr;
-    (*dst_itr)++;
-    struct script_node* this = *dst_itr;
-    prev->next = this;
-    this->prev = prev;
-    this->kind = kind;
-    this->s = s;
-    return this;
-}
 void script_parse_nexttoken(const char** src_itr) {
     if (**src_itr == '(' || **src_itr == ')' || **src_itr == ',') {
         (*src_itr)++;
@@ -48,6 +33,24 @@ void script_parse_nexttoken(const char** src_itr) {
     while (**src_itr == ' ' || **src_itr == '\n') {
         (*src_itr)++;
     }
+}
+void script_parse_push(struct script_node** dst_itr, const char* s, enum script_kind kind) {
+    struct script_node* prev = *dst_itr;
+    (*dst_itr)++;
+    struct script_node* this = *dst_itr;
+    prev->next = this;
+    this->prev = prev;
+    this->kind = kind;
+    this->s = 0;
+    for (int32_t i = 0; s[i] != ' ' && s[i] != '(' && s[i] != ')' && s[i] != ','; i++) {
+        this->s <<= 8;
+        if ('0' <= s[0] && s[0] <= '9') {
+            this->s |= s[i] - '0';
+        } else {
+            this->s |= s[i];
+        }
+    }
+    return;
 }
 void script_parse_expr(struct script_node** dst_itr, const char** src_itr) {
     const char* this_s = *src_itr;
@@ -75,7 +78,38 @@ void script_load(struct script_node* dst, const char* src) {
     const char* src_itr = src;
     dst->kind = script_kind_nop;
     script_parse_expr(&dst_itr, &src_itr);
-    script_parse_push(&dst_itr, NULL, script_kind_null);
+}
+void script_exec(struct script_node* script_node, int32_t* mem) {
+    int32_t* sp = &mem[256];
+    struct script_node* pc = script_node;
+    while (pc != NULL) {
+        switch (pc->kind) {
+            case script_kind_nop:
+                break;
+            case script_kind_push:
+                *(sp++) = pc->s;
+                break;
+            case script_kind_call:
+                switch (pc->s) {
+                    case 7171958:  // mov
+                        mem[sp[-2]] = mem[sp[-1]];
+                        sp -= 2;
+                        break;
+                    case 1836021353:  // movi
+                        mem[sp[-2]] = sp[-1];
+                        sp -= 2;
+                        break;
+                    case 6382692:  // add
+                        mem[sp[-2]] += mem[sp[-1]];
+                        sp -= 1;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+        pc = pc->next;
+    }
 }
 
 int main() {
@@ -91,5 +125,6 @@ int main() {
     write(STDOUT_FILENO, "\n", 1);
 
     script_load(script_node, buf);
+    script_exec(script_node, mem);
     return 0;
 }
