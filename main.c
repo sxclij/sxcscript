58772b3234ecb88c9f674046fdf0504f9c902a45
sxclij <sxclij@gmail.com>
2024-10-02 07:19:48 +0000

    ok 9.txt

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <unistd.h>
 
-#define sxcscript_path "test/8.txt"
+#define sxcscript_path "test/9.txt"
 #define sxcscript_capacity (1 << 16)
 #define sxcscript_buf_capacity (1 << 10)
 
@@ -247,7 +247,7 @@ void sxcscript_parse_expr(struct sxcscript* sxcscript, struct sxcscript_token** 
 }
 void sxcscript_parse(struct sxcscript* sxcscript) {
     struct sxcscript_token* token_itr = sxcscript->token;
-    sxcscript_parse_expr(sxcscript, &token_itr);
+    sxcscript_parse_expr(sxcscript, &token_itr, -1, -1);
 }
 void sxcscript_analyze_primitive(struct sxcscript_node* parsed_begin) {
     for (struct sxcscript_node* parsed_itr = parsed_begin; parsed_itr->kind != sxcscript_kind_null; parsed_itr = parsed_itr->next) {
