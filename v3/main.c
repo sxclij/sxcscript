1d29fe019d291fc6922c847cdbf89d7dff2d696d
sxclij <sxclij@gmail.com>
2024-11-17 03:19:12 +0000

    test

--- v3/main.c
+++ v3/main.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 #define stacksize (128 * 1014 * 1024)
-#define sxcscript_path "test/03.txt"
+#define sxcscript_path "test/04.txt"
 #define sxcscript_mem_size (1 << 20)
 #define sxcscript_compile_size (1 << 20)
 #define sxcscript_buf_size (1 << 10)
